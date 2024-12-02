a61ef583b0a56899d2db38d325ee6f310b66b293
sooshiance <s.katafract@gmail.com>
2024-12-02 16:19:34 +0330

    feat: Dockerize the frontend

--- frontend/src/components/Store/Order.jsx
+++ frontend/src/components/Store/Order.jsx
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 import apiCall from '../../services/apiCall';
+import { useNavigate } from 'react-router-dom';
 
 const Order = () => {
+
+  const [coupon, setCoupon] = useState("");
+
+  const navigate = useNavigate();
+
+  checkCoupon = async () => {
+    // 
+
+    const response = await apiCall.post("");
+
+    if (response.status === 200) {
+      alert("")
+    }
+  }
+
   return (
     <>
 
